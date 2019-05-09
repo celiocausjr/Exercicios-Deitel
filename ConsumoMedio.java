5810069338d97258d79168000c6963eae20a1815
celiocausjr <ccaus2@hotmail.com>
2019-05-09 10:12:57 -0300

    4.17 - Deitel - Consumo Médio

--- ConsumoMedio.java
+++ ConsumoMedio.java
@@ -13,54 +13,47 @@ public class ConsumoMedio {
 		int totalKm = 0;
 		int totalLitros =0;
 		int contViagem = 0;
-	
+		double mediaGeral =0;
+		double mediaViagem = 0;
+		
 		System.out.print("Informe a quilometragem percorrida na viagem ou -1 para sair: ");
 		km = input.nextInt();
+		
+		while(km != -1) {
+			
+		contViagem ++;
 		
 		System.out.print("Informe a quantidade de litros consumidos na viagem: ");
 		litros = input.nextInt();
 		
-		contViagem ++;
+		mediaViagem = (double) km / litros;
 		
-		 double mediaViagem = (double) km / litros;  
+		System.out.printf("Nesta viagem foram gastos %d litros e percorridos %d quilometros.", km, litros);
+		System.out.printf("A média de consumo desta viagem foi de: %.2f km por litro. \n", mediaViagem);
+			
+		totalKm = km + totalKm;
+		totalLitros = litros + totalLitros;
+		mediaGeral += (double) totalKm / totalLitros;
 		
-		System.out.printf("Nessa viagem foram gastos &d, e percorridos %d quilometros.", km, litros);
-		System.out.printf("A média de consumo desta viagem foi de: %.2f ", mediaViagem);
+		System.out.print("Informe a quilometragem percorrida na viagem ou -1 para sair: ");
+		km = input.nextInt();
 		
-		// laço while com flag
 		
-			while (km != -1) {
-						
-			totalKm =+ km;
-			totalLitros =+ litros;
-			mediaViagem = 0;
+		
+		}
+	  
+		// condicional para cálculo da média
 			
-			System.out.print("Informe a quilometragem percorrida na viagem ou -1 para sair: ");
-			km = input.nextInt();
+		if (contViagem != 0) {
 			
-			System.out.print("Informe a quantidade de litros consumidos na viagem: ");
-			litros = input.nextInt();
-			
-			mediaViagem =+ ((double) km / litros);  
-			
-			System.out.printf("Nessa viagem foram gastos %d, e percorridos %d quilometros.", km, litros);
-			System.out.printf("A média de consumo desta viagem foi de: %.2f ", mediaViagem);
-			
-			
-			
-			}
-			
-			// condicional para cálculo da média
-			
-			if (contViagem != 0) {
-			
-				System.out.printf("A média de consumo de todas as viagens foi de: %.2f. ", mediaViagem);
-
-			}else
+		System.out.printf("\nNo total foi(ram) realizada(s) %d viagens. A média de consumo de todas as viagens foi de: %.2f km/l. \n", contViagem, mediaGeral);
+		System.out.printf("O total de quilômetros percorridos foi de %d. Foram gastos %d litros.", totalKm, totalLitros);
+		
+		}else
 				
 			{
 				System.out.println("Não houve nenhuma viagem para apurar!");
 			}
 	}
-
 }
+
